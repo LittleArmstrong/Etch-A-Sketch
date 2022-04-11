e226a0df2477d410c03c82c14ed7bfb42c2d14d9
LittleArmstrong <b.mungan@hotmail.de>
2022-04-11 23:09:23 +0200

    add text field for resolution change
    able to change the resolution of the sketch panel from 1x1 to 100x100 where length = height

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,65 @@
 import { $ } from "./toolbox.mjs";
 
 const sketch_container = $("sketch-container");
-const n_divs_row = 60;
-const n_divs = n_divs_row * n_divs_row; //n_rows == n_columns
-const sketch_size = 600 - 4; //size + border in px
-const element_size = sketch_size / n_divs_row;
 
-for (let i = 0; i < n_divs; i++) {
-   let div = document.createElement("div");
-   div.style.width = element_size + "px";
-   div.style.height = element_size + "px";
-   div.addEventListener("mouseenter", paint_div);
-   sketch_container.appendChild(div);
+//set resolution of panel
+
+const panel_resolution = $("resolution");
+const panel_resolution_copy = $("resolution-copy");
+const min_resolution = 1;
+const max_resolution = 100;
+const sketch_size = 600 - 4; //size + border in px
+
+var n_divs_row = 60; //defauöt
+var n_divs_col = n_divs_row;
+var n_divs = n_divs_row * n_divs_col;
+var element_size = sketch_size / n_divs_row;
+
+panel_resolution.value = n_divs_row;
+panel_resolution_copy.textContent = n_divs_row;
+
+set_resolution();
+
+panel_resolution.addEventListener("change", (event) => {
+   let value = Number(event.target.value);
+   if (value !== NaN && value >= min_resolution && value <= max_resolution) {
+      event.target.value = value;
+      panel_resolution_copy.textContent = value;
+      n_divs_row = value; //defauöt
+      n_divs_col = n_divs_row;
+      n_divs = n_divs_row * n_divs_col;
+      element_size = sketch_size / n_divs_row;
+      set_resolution();
+      paint_div();
+      event.target.blur();
+   }
+});
+
+function set_resolution() {
+   while (sketch_container.firstChild) {
+      sketch_container.removeChild(sketch_container.lastChild);
+   }
+   for (let i = 0; i < n_divs; i++) {
+      let div = document.createElement("div");
+      div.style.width = element_size + "px";
+      div.style.height = element_size + "px";
+      console.log();
+      sketch_container.appendChild(div);
+   }
 }
+
+// painting
+paint_div();
 
 function paint_div(event) {
-   event.target.style.backgroundColor = "black";
+   document.querySelectorAll("#sketch-container>div").forEach((node) => {
+      node.addEventListener("mouseenter", (event) => {
+         event.target.style.backgroundColor = "black";
+      });
+   });
 }
 
+//clearing sketch
 const clear_btn = $("clear-btn");
 clear_btn.addEventListener("click", clear_sketch);
 
