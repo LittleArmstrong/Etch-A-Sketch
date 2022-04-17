e396214dd661049a508ce065f97364031b7b9809
LittleArmstrong <b.mungan@hotmail.de>
2022-04-18 00:08:56 +0200

    add new functions to toolbox
    function assign_object that assigns the values of second object to the first if the keys match and the rules aren't broken
    
    function is_positive_num and is_positive_int to check if positive num/int

--- scripts/main.js
+++ scripts/main.js
@@ -1,71 +1,8 @@
-import { $ } from "./toolbox.mjs";
+import SketchWidget from "./sketch_panel.mjs";
+import { $, $all } from "./toolbox.mjs";
 
-const sketch_container = $("sketch-container");
+const clear_btn = $("#clear-btn");
 
-//set resolution of panel
+clear_btn.addEventListener("click", SketchWidget.clear);
 
-const panel_resolution = $("resolution");
-const panel_resolution_copy = $("resolution-copy");
-const min_resolution = 1;
-const max_resolution = 100;
-const sketch_size = 600 - 4; //size + border in px
-
-var n_divs_row = 60; //defauöt
-var n_divs_col = n_divs_row;
-var n_divs = n_divs_row * n_divs_col;
-var element_size = sketch_size / n_divs_row;
-
-panel_resolution.value = n_divs_row;
-panel_resolution_copy.textContent = n_divs_row;
-
-set_resolution();
-
-panel_resolution.addEventListener("change", (event) => {
-   let value = Number(event.target.value);
-   if (value !== NaN && value >= min_resolution && value <= max_resolution) {
-      event.target.value = value;
-      panel_resolution_copy.textContent = value;
-      n_divs_row = value; //defauöt
-      n_divs_col = n_divs_row;
-      n_divs = n_divs_row * n_divs_col;
-      element_size = sketch_size / n_divs_row;
-      set_resolution();
-      paint_div();
-      event.target.blur();
-   }
-});
-
-function set_resolution() {
-   while (sketch_container.firstChild) {
-      sketch_container.removeChild(sketch_container.lastChild);
-   }
-   for (let i = 0; i < n_divs; i++) {
-      let div = document.createElement("div");
-      div.style.width = element_size + "px";
-      div.style.height = element_size + "px";
-      console.log();
-      sketch_container.appendChild(div);
-   }
-}
-
-// painting
-paint_div();
-
-function paint_div(event) {
-   document.querySelectorAll("#sketch-container>div").forEach((node) => {
-      node.addEventListener("mouseenter", (event) => {
-         event.target.style.backgroundColor = "black";
-      });
-   });
-}
-
-//clearing sketch
-const clear_btn = $("clear-btn");
-clear_btn.addEventListener("click", clear_sketch);
-
-function clear_sketch(event) {
-   let divs = Array.from(document.querySelectorAll("#sketch-container>div"));
-   divs.forEach((node) => {
-      node.style.backgroundColor = "white";
-   });
-}
+SketchWidget.init("#sketch-container");
--- scripts/toolbox.mjs
+++ scripts/toolbox.mjs
@@ -5,5 +5,59 @@ export function get_random_int(min, max) {
 }
 
 export function $(x) {
-   return document.getElementById(x);
+   return document.querySelector(x);
+}
+
+export function $all(x) {
+   return document.querySelectorAll(x);
+}
+
+// strict = true: throw error if key not found in rules
+// alt_obj: object with default values for the keys
+// rules: object with arrays for every key where [function, arg2, arg3, ...]. arg1 ist the value of obj2 that needs to be checked
+// The function should check the validitiy of the values of obj2 and return true or false
+export function assign_object(obj1, obj2, rules = null, strict = true, alt_obj = null) {
+   Object.keys(obj2).forEach((key) => {
+      if (key in obj1) {
+         let value = obj2[key];
+         if (rules !== null) {
+            if (rules.hasOwnProperty(key)) {
+               let args = rules[key].slice(1);
+               let passed = false;
+               if (args.length !== 0) {
+                  passed = rules[key][0](value, ...args);
+               } else {
+                  passed = rules[key][0](value);
+               }
+               if (passed) {
+                  obj1[key] = value;
+               } else if (alt_obj !== null && alt_obj.hasOwnProperty(key)) {
+                  obj1[key] = alt_obj[key];
+               } else {
+                  throw `Invalid value '${value}' for key '${key}'! No default value found.`;
+               }
+            } else if (strict) {
+               throw `No validation rules for key '${key}' found!`;
+            } else {
+               obj1[key] = obj2[key];
+            }
+         } else {
+            obj1[key] = obj2[key];
+         }
+      }
+   });
+}
+
+export function is_positive_num(value, include_zero = true) {
+   if (typeof value === "number") {
+      return value > 0 || (value === 0 && include_zero);
+   }
+   return false;
+}
+
+export function is_positive_int(value, include_zero = true) {
+   if (typeof value === "number") {
+      return (value > 0 && value % 1 === 0) || (value === 0 && include_zero);
+   }
+   return false;
 }
