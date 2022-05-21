471c16a84e4b4ebe26d470cacf7f6e73399147e3
LittleArmstrong <b.mungan@hotmail.de>
2022-05-21 20:48:14 +0200

    document code and rearrange variables

--- scripts/sketch_widget.mjs
+++ scripts/sketch_widget.mjs
@@ -1,30 +1,43 @@
 import { $ } from "./toolbox.mjs";
 
 const canvas_node = $("#sketch-container");
-const resolution_IO = { width: $("#cols"), height: $("#rows") };
-const change_resolution_btn = $("#resize-btn");
-const invalid_resolution_input_css_class = "invalid-number";
-const resolution_settings = {
-   limits: { width: [1, 100], height: [1, 100] },
-   min_size: { width: 1, height: 1 },
-   default: { width: 60, height: 60 },
+const resolution_width = {
+   IO: $("#cols"),
+   limit: [1, 100],
+   min_size: 1,
+   default: 60,
 };
-const paint_color = "black";
+const resolution_height = {
+   IO: $("#rows"),
+   limit: [1, 100],
+   min_size: 1,
+   default: 60,
+};
+/**
+ * Start widget:
+ * - Bind events
+ * - Set canvas to border-box
+ * - Write the default resolution values into the field
+ * - Create resolution grid
+ * - Allow painting
+ */
+export function init_SketchWidget() {
+   bind_events();
+   set_to_border_box(canvas_node);
+   write_to_resolution_field(resolution_width.default, resolution_height.default);
+   change_resolution();
+}
+
+const resolution_btn = $("#resize-btn");
 const clear_btn = $("#clear-btn");
-const clear_color = "white";
 
 let is_painting = false;
-
-const SketchWidget = {
-   init() {
-      bind_events();
-      set_to_border_box(canvas_node);
-      write_to_resolution_field(resolution_settings.default);
-      change_resolution();
-      return this;
-   },
-};
-
+/**
+ * Add all necessary events to the specified nodes.
+ * - Clicking on canvas for painting
+ * - Clicking on clear button to clear canvas
+ * - Clicking on resolution button to change resolution
+ */
 function bind_events() {
    // allow painting only if mouse button is being pressed down
    canvas_node.addEventListener("mousedown", () => {
@@ -35,7 +48,7 @@ function bind_events() {
    });
 
    // add reize function to resize button ("Apply")
-   change_resolution_btn.addEventListener("click", () => {
+   resolution_btn.addEventListener("click", () => {
       change_resolution();
    });
 
@@ -45,20 +58,48 @@ function bind_events() {
    });
 }
 
+/**
+ * Set the content-type of the element to "border-box"
+ *
+ * @param {HTMLElement} node Some HTMLElement
+ */
+
 function set_to_border_box(node) {
    node.setAttribute("box-sizing", "border-box");
 }
 
-function write_to_resolution_field({ width, height }) {
-   write_to_field(height, resolution_IO.height);
-   write_to_field(width, resolution_IO.width);
+/**
+ * Write the values to the fields for the resolution change.
+ *
+ * @param {{width: (number|string), height: (number|string)}} resolution Resolution width and height in cells
+ */
+
+function write_to_resolution_field(width, height) {
+   write_to_field(width, resolution_width.IO);
+   write_to_field(height, resolution_height.IO);
 }
 
+/**
+ * Write the value to the passed input element.
+ *
+ * @param {(string|number)} value   Value to be written
+ * @param {HTMLElement} field       An input element
+ */
 function write_to_field(value, field) {
-   return (field.value = value);
+   field.value = value;
 }
+
+/**
+ * Changes the resolution of the canvas to the value set in the input, if valid.
+ * Basically replaces the div children of the canvas node (div) with the specified amount
+ * of divs (cells) in the input with the cell size calculated depending on the canvas size
+ * and chosen resolution (amount of cells)
+ *
+ * @returns {["ok"|"error, null|string]} Either "ok" with null or "error" with a message
+ */
 
 function change_resolution() {
+   // get the size of the canvas and check if valid
    const canvas_size = get_true_node_size(canvas_node);
    const [canvas_size_status, canvas_size_result] = validate_canvas_size(canvas_size);
    switch (canvas_size_status) {
@@ -70,6 +111,7 @@ function change_resolution() {
          throw new Error("Invalid case");
    }
 
+   // read the input values for the new resolution and check if they are valid
    const resolution = read_from_resolution_field();
    const [resolution_status, resolution_result] = validate_resolution(resolution);
    switch (resolution_status) {
@@ -82,88 +124,153 @@ function change_resolution() {
       default:
          throw new Error("Invalid case");
    }
+   // calcualte the needed amount of cells, their size and create a resolution grid (parent div) out of div elements (children)
    const cell_amount = calc_cell_amount(resolution_result);
+
    const cell_size = calc_cell_size(resolution_result, canvas_size);
+
    const grid = create_grid(cell_amount, cell_size);
+   // replace the children of the canvas node with the children of newly created grid
    set_resolution(grid);
+   // add the ability to paint the canvas through clicking, hovering and by changing the background color
    add_paint_function_to_canvas();
+   return ok(null);
 }
+
+/**
+ * Get the size of a node excluding the border (border-box).
+ *
+ * @param {HTMLElement} node                    The element we want the size of
+ * @returns {{height: number, width: number}}   Available size of the canvas (without border)
+ */
 
 function get_true_node_size(node) {
-   const height = node.clientHeight - 2 * node.style.borderWidth;
    const width = node.clientWidth - 2 * node.style.borderWidth;
-   return { height: height, width: width };
+   const height = node.clientHeight - 2 * node.style.borderWidth;
+   return { width: width, height: height };
 }
 
+/**
+ * Check if the canvas node is above the minimum allowed value
+ *
+ * @param {{width:number, height: number}} canvas_size               Canvas size in px
+ * @returns {["ok"|"error",{width: width, height: height}|string]}  "ok" with canvas size or "error" with message
+ */
 function validate_canvas_size({ width, height }) {
-   const min_canvas = calc_min_canvas_size(
-      resolution_settings.min_size,
-      resolution_settings.limits
-   );
-   if (width < min_canvas.width || height < min_canvas.height) return error("Invalid size");
+   const { min_width, min_height } = calc_min_canvas_size();
+   if (width < min_width || height < min_height) return error("Invalid size");
    return ok({ width: width, height: height });
 }
 
-function calc_min_canvas_size(min_cell_size, resolution_limits) {
-   const min_canvas = {};
-   min_canvas.width = min_cell_size.width * resolution_limits.width[1];
-   min_canvas.height = min_cell_size.height * resolution_limits.height[1];
-   return min_canvas;
+/**
+ * Calculate the smallest allowed width and height of the canvas based on the maximum allowed amount of cells
+ * and the lowest allowed cell width and height
+ *
+ * @returns {{width: number, height: number}} Object with the smallest allowed width and height of the canvas in px
+ */
+
+function calc_min_canvas_size() {
+   const width = calc_min_dim(width_settings.min_size, width_settings.limit[1]);
+   const height = calc_min_dim(height_settings.min_size, height_settings.limit[1]);
+   return { min_width: width, min_height: height };
 }
 
+/**
+ * Calculate the smallest size needed for the specified values.
+ *
+ * @param {number} min_value Smallest allowed value of the unit
+ * @param {number} max_amount Highest allowed amount allowed of the value
+ * @returns {number} Smallest needed size to allow the specified values
+ */
+
+function calc_min_dim(min_value, max_amount) {
+   return min_value * max_amount;
+}
+
+/**
+ * Read the values of the resolution input fields and convert them to numbers.
+ *
+ * @returns {{width: number, height: number}} Resolution in cells
+ */
 function read_from_resolution_field() {
-   const width = read_from_field_as_number(resolution_IO.width);
-   const height = read_from_field_as_number(resolution_IO.height);
+   const width = read_from_field_as_number(resolution_width.IO);
+   const height = read_from_field_as_number(resolution_height.IO);
    return { width: width, height: height };
 }
+
+/**
+ * Read the value of an input type element
+ *
+ * @param {HTMLElement} field    An input type element
+ * @returns {number}             The value converted into a number
+ */
 
 function read_from_field_as_number(field) {
    return Number(field.value);
 }
 
-function numbers_are_integer(numbers) {
-   Object.values(numbers).forEach((number) => {
-      if (!Number.isInteger(number)) return false;
-   });
-   return true;
+/**
+ * Check if the resolution values are integers and in the allowed range
+ *
+ * @param {{width: number, height: number}} resolution  Contains the width and height of the canvas resolution in px
+ * @returns {["ok"|"error", object|string]}             "ok" with resolution or "error" with message
+ */
+function validate_resolution({ width, height }) {
+   if (!Number.isInteger(width) || !Number.isInteger(height)) return error("Not an integer");
+   if (width < resolution_width.limit[0] || width > resolution_width.limit[1])
+      return error("Resolution width not in range");
+   if (height < resolution_height.limit[0] || height > resolution_height.limit[1])
+      return error("Resolution height not in range");
+   return ok({ width, height });
 }
 
-function numbers_are_in_range(numbers, limits) {
-   Object.entries(numbers).forEach(([key, value]) => {
-      if (value < limits[key][0] || value > limits[key][1]) return false;
-   });
-   return true;
-}
-
-function validate_resolution(resolution) {
-   if (
-      !numbers_are_integer(resolution) ||
-      !numbers_are_in_range(resolution, resolution_settings.limits)
-   )
-      return error("Invalid resolution");
-   return ok(resolution);
-}
-
+const invalid_resolution_input_css_class = "invalid-number";
+/**
+ * Remove the CSS class that adds red borders from the resolution input field
+ */
 function show_valid_resolution_input() {
-   resolution_IO.width.classList.remove(invalid_resolution_input_css_class);
-   resolution_IO.height.classList.remove(invalid_resolution_input_css_class);
-}
-function show_invalid_resolution_input() {
-   resolution_IO.width.classList.add(invalid_resolution_input_css_class);
-   resolution_IO.height.classList.add(invalid_resolution_input_css_class);
+   resolution_width.IO.classList.remove(invalid_resolution_input_css_class);
+   resolution_height.IO.classList.remove(invalid_resolution_input_css_class);
 }
 
+/**
+ * Add the CSS class that adds red borders to the resolution input field
+ */
+function show_invalid_resolution_input() {
+   resolution_width.IO.classList.add(invalid_resolution_input_css_class);
+   resolution_height.IO.classList.add(invalid_resolution_input_css_class);
+}
+
+/**
+ * Multiply the cell amount in the width and height for the total amount
+ *
+ * @param {{width: number, height: number}} resolution   Resolution
+ * @returns {number}                                     The total cell amount
+ */
 function calc_cell_amount({ width, height }) {
    return width * height;
 }
 
+/**
+ * Calculate the width and height of the cells for the given resolution and canvas size
+ *
+ * @param {{width: number, height: number}} resolution   Resolution size in cells
+ * @param {{width: number, height: number}} canvas_size  Canvas size in px
+ * @returns {{width: number, height: number}}            Returns cell size in px
+ */
 function calc_cell_size(resolution, canvas_size) {
-   const cell_size = {};
-   cell_size.width = canvas_size.width / resolution.width;
-   cell_size.height = canvas_size.height / resolution.height;
-   return cell_size;
+   const width = canvas_size.width / resolution.width;
+   const height = canvas_size.height / resolution.height;
+   return { width: width, height: height };
 }
 
+/**
+ * Create a div with the specified amount of div children in a specified size.
+ *
+ * @param {number} cell_amount                        Amount of div children (cells of the grid)
+ * @param {{width: number, height: number}} cell_size Size of the div children (size of the grid cells)
+ * @returns {HTMLElement}                             div element with div children
+ */
 function create_grid(cell_amount, cell_size) {
    const grid = document.createElement("div");
    for (let i = 0; i < cell_amount; i++) {
@@ -177,10 +284,19 @@ function create_grid(cell_amount, cell_size) {
    return grid;
 }
 
+/**
+ * Replace the children of the canvas node with the provided ones
+ *
+ * @param {HTMLElement} grid  div with child elements
+ */
 function set_resolution(grid) {
    canvas_node.replaceChildren(...grid.children);
 }
 
+const paint_color = "black";
+/**
+ * Allow the painting on click over the canvas
+ */
 function add_paint_function_to_canvas() {
    Array.from(canvas_node.children).forEach((cell) => {
       cell.addEventListener("mouseover", (event) => {
@@ -189,18 +305,32 @@ function add_paint_function_to_canvas() {
    });
 }
 
+const clear_color = "white";
+/**
+ * Fill the canvas with the specified color for clearing
+ */
 function clear() {
    Array.from(canvas_node.children).forEach((cell) => {
       cell.style.backgroundColor = clear_color;
    });
 }
 
+/**
+ * Used as a return value to indicate that everythin went proper
+ *
+ * @param {any} result     The value of a sucessful called function
+ * @returns {["ok", any]}  OK status with value
+ */
 function ok(result) {
    return ["ok", result];
 }
 
+/**
+ * Used a return value to indicate that something went wrong
+ *
+ * @param {string} reason        reason for failure
+ * @returns {["error", string]}  Error status with message
+ */
 function error(reason) {
    return ["error", reason];
 }
-
-export default SketchWidget;
